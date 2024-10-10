4d51da378daaa25606c02ca4842efcd09ab2f248
arseniyabramov <abramovarsenij2005@gmail.com>
2024-10-10 18:22:24 +0300

    done section Hero
    with adaptive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,8 +9,10 @@ function Hero() {
 			<div className='hero__content container'>
 				<h3 className='hero__subtitle'>Привет, Я</h3>
 				<h2 className='hero__name'>Вячеслав Меломатов</h2>
-				<h1 className='hero__title'>Тату мастер</h1>
-				<h3 className='hero__location'>Работаю в Москве</h3>
+				<div className='hero__info'>
+					<h1 className='hero__title'>Тату мастер</h1>
+					<h3 className='hero__location'>Работаю в Москве</h3>
+				</div>
 			</div>
 			<Slider />
 		</section>
