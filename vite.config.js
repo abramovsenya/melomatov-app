1a8387d92d196d9b8e7f002d5313f940b589ae7f
arseniyabramov <abramovarsenij2005@gmail.com>
2024-10-12 02:11:50 +0300

    add: package.json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/melomatov-app/',
 	server: {
 		host: true, // Это позволит доступ к серверу через IP в локальной сети
 		port: 3000, // Укажи любой порт, который тебе нужен (можно оставить 3000)
